8b4a81017e13befd8323b372ee64186e720803c5
Beth <bethhnielsen56@gmail.com>
2020-02-29 11:00:45 -0600

    graveyard working

---
+++ javascripts/components/graveyard.js
@@ -0,0 +1,21 @@
+import utils from '../helpers/utils.js';
+import personData from '../helpers/data/personData.js';
+
+const buildGraveyard = () => {
+    const persons = personData.getDeadPersons();
+    let domString = "";
+    domString += `<h1 class="text-center m-3">Grave Yard</h1>`;
+    domString += `<div class="card w-75 mx-auto">`;
+    domString += `<ul class="list-group list-group-flush">`;
+    persons.forEach((person) => {
+        domString += `<li class="list-group-item text-center">${person.name}</li>`;
+    });
+  
+    domString += `</ul>`;
+    domString += `</div>`; 
+
+    utils.printToDom("graveyard", domString);
+}
+
+
+export default { buildGraveyard }
