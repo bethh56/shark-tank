a9e36308104a551b4842025db6c98f5e431c6470
Beth <bethhnielsen56@gmail.com>
2020-02-29 10:30:12 -0600

    wip

--- javascripts/components/tank.js
+++ javascripts/components/tank.js
@@ -1,9 +1,20 @@
 import utils from "../helpers/utils.js"
-// import personData from "../helpers/data/personData.js"
+import personData from "../helpers/data/personData.js"
 
 const tankBuilder = () => {
-    let domString = "help";
-    
+    const persons = personData.getAlivePersons();
+    let domString = "";
+    domString += `<div class="card w-75 mx-auto">`;
+    domString += `<div class="card-header">Featured</div>`;
+    domString += `<ul class="list-group list-group-flush">`;
+
+    persons.forEach((person) => {
+        domString += `<li class="list-group-item">${person.name}</li>`;
+    });
+  
+    domString += `</ul>`;
+    domString += `</div>`; 
+
     utils.printToDom("tank", domString);
 }
 
